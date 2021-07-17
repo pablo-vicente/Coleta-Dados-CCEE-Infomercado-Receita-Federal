1d43a0a8b33374a9c9fc8417cbf81eb58f96519d
Pablo Vicente <pablo.vicente@outlook.com.br>
2021-07-17 14:33:04 -0300

    FIX FK RRH

---
+++ src/Services/Infomercado.Domain/Migrations/20210717171630_FixFK.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infomercado.Domain.Migrations
+{
+    public partial class FixFK : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropUniqueConstraint(
+                name: "AK_RepasseRiscoHidrologicos_Mes_IdPerfilAgente_IdParcelaUsina_Patamar_Semana",
+                table: "RepasseRiscoHidrologicos");
+
+            migrationBuilder.AddUniqueConstraint(
+                name: "AK_RepasseRiscoHidrologicos_Mes_IdPerfilAgente_IdParcelaUsina_Patamar_Semana_TipoRepasseRiscoHidrologico",
+                table: "RepasseRiscoHidrologicos",
+                columns: new[] { "Mes", "IdPerfilAgente", "IdParcelaUsina", "Patamar", "Semana", "TipoRepasseRiscoHidrologico" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropUniqueConstraint(
+                name: "AK_RepasseRiscoHidrologicos_Mes_IdPerfilAgente_IdParcelaUsina_Patamar_Semana_TipoRepasseRiscoHidrologico",
+                table: "RepasseRiscoHidrologicos");
+
+            migrationBuilder.AddUniqueConstraint(
+                name: "AK_RepasseRiscoHidrologicos_Mes_IdPerfilAgente_IdParcelaUsina_Patamar_Semana",
+                table: "RepasseRiscoHidrologicos",
+                columns: new[] { "Mes", "IdPerfilAgente", "IdParcelaUsina", "Patamar", "Semana" });
+        }
+    }
+}
